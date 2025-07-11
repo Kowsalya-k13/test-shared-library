7f84a5418620a23d8d971e89def0ebd6d9d0afb8
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-07-11 13:01:12 +0530

    Added diff def - for checking

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,3 +1,6 @@
-def call(String buildCommand = 'mvn clean install') {
-    sh "${buildCommand}"
+// def call(String buildCommand = 'mvn clean install') {
+//     sh "${buildCommand}"
+// }
+def call() {
+    echo "Second stage - Completed"
 }
--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,3 +1,6 @@
-def call(String deployCommand = './deploy.sh') {
-    sh "${deployCommand}"
+// def call(String deployCommand = './deploy.sh') {
+//     sh "${deployCommand}"
+// }
+def call() {
+    echo "Third stage - Completed"
 }
