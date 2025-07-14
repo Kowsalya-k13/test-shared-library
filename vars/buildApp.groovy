e1fc65f09b9019edb323fc9b4aba524db148624b
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-07-14 12:55:09 +0530

    Made changes to func files

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,3 @@
-// def call(String buildCommand = 'mvn clean install') {
-//     sh "${buildCommand}"
-// }
 def call() {
     echo "Second stage - Completed"
 }
--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,3 @@
-// def call(String deployCommand = './deploy.sh') {
-//     sh "${deployCommand}"
-// }
 def call() {
     echo "Third stage - Completed"
 }
